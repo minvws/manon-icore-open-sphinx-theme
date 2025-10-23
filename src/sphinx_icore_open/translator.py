607d8acbc56cd6964fdcb3b3eeb72372b54ad5bd
rievenmartis <rsnmartis@gmail.com>
2025-10-23 11:21:00 +0200

    fix translator

--- src/sphinx_icore_open/translator.py
+++ src/sphinx_icore_open/translator.py
@@ -8,7 +8,7 @@ def register_translator(app):
     from sphinx.writers.html import HTMLTranslator
 
     class CustomHTMLTranslator(HTMLTranslator):
-        def visit_title(self, node):
+        def visit_title(self, node) -> None:
             """Overrides the default title visitor.
             This method checks if a title is inside an admonition. If it is,
             it wraps the title text in a `<span>` tag to allow for custom
