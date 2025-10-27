b5d464405cfe6e34c28eb7d125f006d1be3bdb9f
JP Bruins Slot <jpbruinsslot@gmail.com>
2025-10-27 08:12:39 +0100

    Restructure

--- src/sphinx_icore_open/__init__.py
+++ src/sphinx_icore_open/__init__.py
@@ -1,18 +1,18 @@
 import os
 from importlib.metadata import version
 
+from sphinx.application import Sphinx
+
+from sphinx_icore_open import translator
+
 __version__ = version("sphinx_icore_open")
 
 
-def setup(app):
+def setup(app: Sphinx):
     root_path = os.path.abspath(os.path.dirname(__file__))
     theme_path = os.path.join(root_path, "theme/sphinx_icore_open")
 
     app.add_html_theme("sphinx_icore_open", theme_path)
-
-    # Lazy import and register translator
-    from .translator import register_translator
-
-    register_translator(app)
+    app.set_translator("html5", translator.CustomHTMLTranslator)
 
     return {"parallel_read_safe": True, "parallel_write_safe": True}
--- src/sphinx_icore_open/translator.py
+++ src/sphinx_icore_open/translator.py
@@ -1,27 +1,19 @@
-def register_translator(app) -> None:
-    """
-    Register the custom HTML translator with Sphinx.
-    """
+from docutils import nodes
+from sphinx.application import Sphinx
+from sphinx.writers.html import HTMLTranslator
 
-    # Import here to avoid ImportError when installing the package without Sphinx
-    from docutils import nodes
-    from sphinx.writers.html import HTMLTranslator
 
-    class CustomHTMLTranslator(HTMLTranslator):
-        def visit_title(self, node) -> None:
-            """Overrides the default title visitor.
-            This method checks if a title is inside an admonition. If it is,
-            it wraps the title text in a `<span>` tag to allow for custom
-            styling, and then prevents further processing of the node.
-            For all other titles, it calls the original `visit_title` method
-            to ensure default rendering.
-            """
-            if isinstance(node.parent, nodes.Admonition):
-                self.body.append(f"<span>{node.astext()}</span>")
-                raise nodes.SkipNode
-            else:
-                super().visit_title(node)
-
-    # Register translator for all HTML builders
-    app.set_translator("html", CustomHTMLTranslator)
-    app.set_translator("html5", CustomHTMLTranslator)
+class CustomHTMLTranslator(HTMLTranslator):
+    def visit_title(self, node) -> None:
+        """Overrides the default title visitor.
+        This method checks if a title is inside an admonition. If it is,
+        it wraps the title text in a `<span>` tag to allow for custom
+        styling, and then prevents further processing of the node.
+        For all other titles, it calls the original `visit_title` method
+        to ensure default rendering.
+        """
+        if isinstance(node.parent, nodes.Admonition):
+            self.body.append(f"<span>{node.astext()}</span>")
+            raise nodes.SkipNode
+        else:
+            super().visit_title(node)
