439751591c8f4e32b122a55e8c34ffba827dac4e
JP Bruins Slot <jpbruinsslot@gmail.com>
2025-10-28 13:51:30 +0100

    Fix loading setup() locally

--- src/sphinx_icore_open/__init__.py
+++ src/sphinx_icore_open/__init__.py
@@ -7,11 +7,12 @@ from sphinx_icore_open import translator
 
 __version__ = version("sphinx-icore-open-theme")
 
+
 def setup(app: Sphinx):
     root_path = os.path.abspath(os.path.dirname(__file__))
     theme_path = os.path.join(root_path, "theme/sphinx_icore_open")
 
     app.add_html_theme("sphinx_icore_open", theme_path)
-    app.set_translator("html5", translator.CustomHTMLTranslator)
+    app.set_translator("html", translator.CustomHTMLTranslator)
 
     return {"parallel_read_safe": True, "parallel_write_safe": True}
--- src/sphinx_icore_open/translator.py
+++ src/sphinx_icore_open/translator.py
@@ -6,6 +6,7 @@ from sphinx.writers.html import HTMLTranslator
 class CustomHTMLTranslator(HTMLTranslator):
     def visit_title(self, node) -> None:
         """Overrides the default title visitor.
+
         This method checks if a title is inside an admonition. If it is,
         it wraps the title text in a `<span>` tag to allow for custom
         styling, and then prevents further processing of the node.
